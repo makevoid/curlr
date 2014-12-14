57757c8ddf8a9ea95600d4493ff9f296eb03747c
Francesco (makevoid) <makevoid@gmail.com>
2014-12-14 23:15:33 +0000

    simple cli tool

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Curlr
+
+### Curl-like CLI tool wri
+
+    curlr http://emberjs.com
+
+alias for:
+
+    curlr get http://emberjs.com
+
+then you can pass parameters with get:
+
+    curlr get http://jquery.com/?wathever=tue
+
+
+and of course, make a post request
+
+    curlr post http://jquery.com/?wathever=tue
+
+
+### todo:
+
+- put,delete,patch,head
+- redirect
+- cache?
---
+++ curlr.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+require "net/http"
+
+class Curlr
+  def self.get(url)
+    new(url).get
+  end
+
+  def self.post(url)
+    new(url).post
+  end
+
+  def initialize(url)
+    @uri = URI.parse url
+  end
+
+  def get
+    resp = Net::HTTP.get_response @uri
+    resp.body
+  end
+
+  def post
+    params = CGI.parse URI.parse(@uri).query
+    resp = Net::HTTP.post_form @uri, params
+    resp.body
+  end
+end
+
+def resolve(method:, url:)
+  case method
+  when :get
+    Curlr.get  url
+  when :post
+    Curlr.post url
+  when :put
+    raise "Not implemented, sorry :)"
+  when :delete
+    raise "Not implemented, sorry :)"
+  end
+end
+
+def main
+  method = "get"
+  url    = ARGV[0]
+  if ["get", "post"].include? ARGV[0]
+    method = ARGV[0]
+    url    = ARGV[1]
+  end
+  method = method.to_sym
+
+  resolve method: method, url: url
+end
+
+
+puts main
